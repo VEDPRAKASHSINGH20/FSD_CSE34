e7b4d6a8911cffa99f44a0c8330b82ad91b0a866
VED PRAKASH SINGH <vedprakashs20595@gmail.com>
2026-07-16 16:17:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function lineofstars(){
+    for(let i=0; i<5;i++){
+        process.stdout.write("*");
+    }
+    console.log();
+}
+lineofstars();
+console.log("Hello");
+lineofstars();
+console.log("world");
+lineofstars();
